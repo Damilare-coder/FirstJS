ee9baf1040683787e395c04fa07f0538c1ad5070
Damilare Oyinloye <oyinloyedamilare12@gmail.com>
2025-03-26 14:56:55 +0100

    I learnt JavaScript Data types and arithmetic operations

--- script.js
+++ script.js
@@ -1,4 +1,99 @@
-alert(300)
+// alert(300)
 // alert only works with numbers
 
 console.log(100)
+
+
+// JavaScript Class 2 - 26/03/2025
+
+// Data Types
+console.log( `Jide123`) 
+// string
+
+console.log(334) 
+// number
+
+console.log( false); 
+// boolean
+
+console.log(typeof(false)); 
+// typeof is used to check what data type a value is
+
+
+// Variable
+
+var studentThatCameLate = 'Sultan'
+
+console.log(studentThatCameLate);
+
+// Operators in JS
+// Arithmetic Operator
+
+console.log(2==='2');
+
+console.log('Area of Triangle');
+var base = 6
+var height = 5
+
+var areaOfTriangle = (1/2 * (base * height))
+console.log(areaOfTriangle);
+
+
+// Assignment: Find area of the below shapes
+
+
+
+// Area of Sqaure
+console.log('Area of Square');
+
+var l = 5
+
+var areaOfSquare = l**2
+console.log(areaOfSquare);
+
+// Area of Circle
+console.log('Area of Circle');
+
+var Pi = 3.14
+var r = 200
+
+var areaOfCircle = Pi * r**2
+console.log(areaOfCircle);
+
+// Area of Rhombus
+console.log('Area of Rhombus');
+
+var d1 = 5
+var d2 = 6
+
+var areaOfRhombus = 1/2 * d1*d2
+console.log(areaOfRhombus);
+
+// Area of Trapezium
+console.log('Area of Trapezium');
+
+var base1 = 5
+var base2 = 6
+let h = 10
+
+var areaOfTrapezium = (1/2 * (base1+base2) * h)
+console.log(areaOfTrapezium);
+
+// Area of Parallelogram
+
+console.log('Area of Parallelogram');
+var base = 6
+var height = 5
+
+var areaOfParallelogram = (base * height)
+console.log(areaOfParallelogram);
+
+
+// Calculate body mass index (BMI)
+
+console.log('Body Mass Index');
+var weight = 70
+var height = 2.2
+
+var bodyMassIndex = (weight / height**2)
+console.log(bodyMassIndex);
